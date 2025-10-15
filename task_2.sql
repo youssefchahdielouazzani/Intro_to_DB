d85fcda90e9696613d005b8ec8eb50b7dc4af84f
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-10-15 15:40:38 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -34,7 +34,7 @@ CREATE TABLE Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
